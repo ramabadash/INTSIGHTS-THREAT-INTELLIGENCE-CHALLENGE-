8ce76db676fda3bb00e32b7ed87a3ae57c2a846e
ramabadash <89576646+ramabadash@users.noreply.github.com>
2022-02-06 19:13:52 +0200

    ⚙ Get a paste and retrun object with "Title" , "Author" and "Date" (Not content yet)

--- webScraper.py
+++ webScraper.py
@@ -1,3 +1,4 @@
+from logging import error
 import requests
 from bs4 import BeautifulSoup
 
@@ -9,7 +10,6 @@ def get_landing_page(url, socks_proxy, http_proxy):
     http_proxy: users http_proxy str
     return: Landing html page for url
     """
-    print("In the function")
     proxies = {
         "socks5h": socks_proxy,
         "http": http_proxy,
@@ -21,3 +21,21 @@ def get_landing_page(url, socks_proxy, http_proxy):
     except: 
         print(f"Error with getting {url} page code")
 
+# ---------- GET PASTE AND RETURN AS OBJ ---------- #
+def from_paste_to_object(paste):
+    """
+    :paste: Div element
+    :return: Paste obj with {Title: ? , Author: ?, Date: ?, Content: ?} or None
+    """
+    try:
+        details_line_list = paste.find_all('div', {"class": 'col-sm-6'})[0].text.split(" ")
+        return {
+        "Title": paste.find_all("h4")[0].text.strip("\n").strip("\t"),
+        "Author": details_line_list[2],
+        # "Content":,
+        "Date": " ".join(details_line_list[4:8]),
+        }
+    except:
+        return
+
+print(main())
