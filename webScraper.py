7a78ccb373a7ba66e07fd8fb3c89cd599751f33d
ramabadash <89576646+ramabadash@users.noreply.github.com>
2022-02-06 19:55:15 +0200

    ⚙Get paste content by taking him from full paste page

--- webScraper.py
+++ webScraper.py
@@ -2,6 +2,11 @@ from logging import error
 import requests
 from bs4 import BeautifulSoup
 
+proxies = {
+    "socks5h": "socks5h://127.0.0.1:9050",
+    "http": "http://127.0.0.1:8118",
+}
+
 # ---------- GET LANDING PAGE FOR WEBSITE WITH URL ---------- #
 def get_landing_page(url, socks_proxy, http_proxy):
     """
@@ -33,8 +38,9 @@ def get_results_to_list(html_code):
     for paste in pastes:
         parsed_obj = from_paste_to_object(paste)
         if parsed_obj:
+            print(f"\n\n parsed_obj: \n\n {parsed_obj}")
             pastes_list.append(parsed_obj)
-    print(f"pastes_list :{pastes_list }\n")
+    # print(f"pastes_list :{pastes_list }\n")
     return pastes_list
         
 
@@ -46,14 +52,29 @@ def from_paste_to_object(paste):
     """
     try:
         details_line_list = paste.find_all('div', {"class": 'col-sm-6'})[0].text.split(" ")
+        full_paste_url = paste.find_all('a', {"class": 'btn-success'})[0].attrs["href"]
         return {
         "Title": paste.find_all("h4")[0].text.strip("\n").strip("\t"),
         "Author": details_line_list[2],
-        # "Content":,
+        "Content": get_paste_content(full_paste_url),
         "Date": " ".join(details_line_list[4:8]),
         }
     except:
         return
+
+# ---------- GET PASTE CONTENT ---------- #
+def get_paste_content(full_paste_url):
+    """
+    :full_paste_url: Dark web url leading to full page
+    :return: content str
+    """
+    response = requests.get(full_paste_url, proxies=proxies)
+    full_paste_html = BeautifulSoup(response.text, 'html.parser')
+    all_content = full_paste_html.find_all("li")
+    content = ""
+    for li in all_content:
+        content += f"{li.text}. "
+    return content.strip("\n")
 
 # ---------- MAIN ---------- #
 
@@ -61,4 +82,4 @@ def main():
     html_page = get_landing_page("http://strongerw2ise74v3duebgsvug4mehyhlpa7f6kfwnas7zofs3kov7yd.onion/all", "socks5h://127.0.0.1:9050", "http://127.0.0.1:8118")
     return get_results_to_list(html_page)
 
-print(main())
+main()
